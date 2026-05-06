a0f054e2a6a2754e05778143260f3acfadd52067
Tamas Cservenak <tamas@cservenak.net>
2026-05-06 11:43:35 +0200

    More tools

--- tools/gradle/src/main/java/eu/maveniverse/maven/toolrunner/tools/gradle/GradleProvider.java
+++ tools/gradle/src/main/java/eu/maveniverse/maven/toolrunner/tools/gradle/GradleProvider.java
@@ -5,10 +5,11 @@
  * which accompanies this distribution, and is available at
  * https://www.eclipse.org/legal/epl-v20.html
  */
-package eu.maveniverse.maven.toolrunner.tools.maven;
+package eu.maveniverse.maven.toolrunner.tools.gradle;
 
 import static java.util.Objects.requireNonNull;
 
+import eu.maveniverse.maven.shared.core.fs.FileUtils;
 import eu.maveniverse.maven.toolrunner.shared.ToolExecution;
 import eu.maveniverse.maven.toolrunner.shared.ToolHandle;
 import eu.maveniverse.maven.toolrunner.shared.ToolHandler;
@@ -22,6 +23,7 @@ import eu.maveniverse.maven.toolrunner.shared.support.ProcessBuilderExecutor;
 import eu.maveniverse.maven.toolrunner.shared.support.Provisioners;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.net.URI;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
@@ -32,18 +34,20 @@ import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Stream;
-import org.eclipse.aether.artifact.Artifact;
+import javax.swing.*;
 
 /**
- * The Apache Maven tool provider.
+ * The Gradle tool provider.
  */
-public class MavenProvider implements ToolProvider, ToolDetector, ToolProvisioner, ToolExecutor {
-    public static final String NAME = "maven";
+public class GradleProvider implements ToolProvider, ToolDetector, ToolProvisioner, ToolExecutor {
+    public static final String NAME = "gradle";
 
-    private static final String EXE_NAME = IS_WINDOWS ? "mvn.cmd" : "mvn";
+    private static final String EXE_NAME = IS_WINDOWS ? "gradle.bat" : "gradle";
     private static final String PREFIX = NAME + ".";
 
     public static final String HOME = PREFIX + "home";
+
+    private static final String ENV_HOME = "GRADLE_HOME";
 
     // ToolProvider
 
@@ -76,10 +80,10 @@ public class MavenProvider implements ToolProvider, ToolDetector, ToolProvisione
         // detect from path; if allowed
         if (context.allowPathDetection()) {
             Set<Path> paths =
-                    OSTools.dereference(OSTools.which(MavenProvider.EXE_NAME).orElse(Set.of()));
+                    OSTools.dereference(OSTools.which(GradleProvider.EXE_NAME).orElse(Set.of()));
             // consider only those ending with `bin/$EXE_NAME`
             for (Path executable : paths) {
-                if (executable.toString().replace("\\", "/").endsWith("/bin/" + MavenProvider.EXE_NAME)) {
+                if (executable.toString().replace("\\", "/").endsWith("/bin/" + GradleProvider.EXE_NAME)) {
                     tryHome(context, executable.getParent().getParent()).ifPresent(detected::add);
                 }
             }
@@ -102,34 +106,55 @@ public class MavenProvider implements ToolProvider, ToolDetector, ToolProvisione
     }
 
     /**
-     * Collects basic information about discovered Maven home.
+     * Collects basic information about discovered JBang home.
      */
-    protected Optional<Map<String, String>> tryHome(ToolContext context, Path mavenHome) throws IOException {
+    protected Optional<Map<String, String>> tryHome(ToolContext context, Path jBangHome) throws IOException {
         ByteArrayOutputStream out = new ByteArrayOutputStream();
         ByteArrayOutputStream err = new ByteArrayOutputStream();
 
-        Optional<Map<String, String>> maybeExisting = Provisioners.loadMetadata(context, mavenHome);
+        Optional<Map<String, String>> maybeExisting = Provisioners.loadMetadata(context, jBangHome);
         if (maybeExisting.isPresent()) {
             return maybeExisting;
         }
 
         // execute and discover information
-        Map<String, String> metadata = new HashMap<>(Map.of(MavenProvider.HOME, mavenHome.toString()));
+        Map<String, String> metadata = new HashMap<>(Map.of(GradleProvider.HOME, jBangHome.toString()));
         ToolExecution.Builder exe = executionTemplate(context, metadata)
-                .addArguments("-v", "-q")
+                .addArguments("-v")
                 .stdOut(out)
                 .stdErr(err);
+        // TODO: maybe collect more info?
+        // $ gradle -v
+        //
+        // ------------------------------------------------------------
+        // Gradle 9.5.0
+        // ------------------------------------------------------------
+        //
+        // Build time:    2026-04-28 12:05:30 UTC
+        // Revision:      3fe117d68f3907790f3809f121aa36303a9151f8
+        //
+        // Kotlin:        2.3.20
+        // Groovy:        4.0.29
+        // Ant:           Apache Ant(TM) version 1.10.15 compiled on August 25 2024
+        // Launcher JVM:  21.0.11 (Eclipse Adoptium 21.0.11+10-LTS)
+        // Daemon JVM:    /home/cstamas/.sdkman/candidates/java/21.0.11-tem (no Daemon JVM specified, using current Java
+        // home)
+        // OS:            Linux 6.19.14-200.fc43.x86_64 amd64
+        //
+        // $
 
         try {
             ToolHandle.Result result = executeTool(context, metadata, exe.build());
             if (result.success()) {
+                String[] versions = out.toString().trim().split("\\s");
+                String version = versions[3];
                 return Optional.of(Map.of(
                         ToolHandler.TOOL_NAME,
-                        MavenProvider.NAME,
+                        GradleProvider.NAME,
                         ToolHandler.TOOL_VERSION,
-                        out.toString().trim(),
-                        MavenProvider.HOME,
-                        mavenHome.toString()));
+                        version.trim(),
+                        GradleProvider.HOME,
+                        jBangHome.toString()));
             }
             return Optional.empty();
         } catch (InterruptedException e) {
@@ -144,36 +169,37 @@ public class MavenProvider implements ToolProvider, ToolDetector, ToolProvisione
             throws IOException {
         boolean isLatest = !metadata.containsKey(ToolHandler.TOOL_VERSION);
 
+        String uri;
         String homePath;
-        String version;
+        String version = null;
         if (isLatest) {
-            version = Provisioners.RELEASE_VERSION;
-            homePath = NAME;
+            // TODO: discover
+            version = "9.5.0";
+            uri = String.format("https://services.gradle.org/distributions/gradle-%s-bin.zip", version);
+            homePath = NAME + "-" + version;
         } else {
             version = requireNonNull(metadata.get(ToolHandler.TOOL_VERSION));
+            uri = String.format("https://services.gradle.org/distributions/gradle-%s-bin.zip", version);
             homePath = NAME + "-" + version;
         }
         Path installDir = context.installationDirectory().resolve(homePath);
-        Optional<Artifact> maybeDistro =
-                Provisioners.resolveArtifact(context, "org.apache.maven:apache-maven:zip:bin:" + version);
-        if (maybeDistro.isPresent()) {
-            Provisioners.unpack(context, maybeDistro.orElseThrow().getFile().toPath(), installDir, false);
-            Optional<Map<String, String>> provisioned = tryHome(context, installDir);
-            if (provisioned.isPresent()) {
-                Map<String, String> provisionedMetadata = new HashMap<>(provisioned.orElseThrow());
-                if (Provisioners.RELEASE_VERSION.equals(version)) {
-                    Path versionedInstallDir = context.installationDirectory()
-                            .resolve(NAME + "-" + requireNonNull(provisionedMetadata.get(ToolHandler.TOOL_VERSION)));
-                    Files.move(installDir, versionedInstallDir, StandardCopyOption.REPLACE_EXISTING);
-                    provisionedMetadata.put(HOME, versionedInstallDir.toString());
-                    installDir = versionedInstallDir;
-                }
-                Provisioners.saveMetadata(context, installDir, provisionedMetadata);
-                return Optional.of(provisionedMetadata);
-            }
-            return provisioned;
+        try (FileUtils.TempFile dl = Provisioners.httpGet(context, "services.gradle.org", URI.create(uri))) {
+            Provisioners.unpack(context, dl.getPath(), installDir, false);
         }
-        return Optional.empty();
+        Optional<Map<String, String>> provisioned = tryHome(context, installDir);
+        if (provisioned.isPresent()) {
+            Map<String, String> provisionedMetadata = new HashMap<>(provisioned.orElseThrow());
+            if (version == null) {
+                Path versionedInstallDir = context.installationDirectory()
+                        .resolve(NAME + "-" + requireNonNull(provisionedMetadata.get(ToolHandler.TOOL_VERSION)));
+                Files.move(installDir, versionedInstallDir, StandardCopyOption.REPLACE_EXISTING);
+                provisionedMetadata.put(HOME, versionedInstallDir.toString());
+                installDir = versionedInstallDir;
+            }
+            Provisioners.saveMetadata(context, installDir, provisionedMetadata);
+            return Optional.of(provisionedMetadata);
+        }
+        return provisioned;
     }
 
     // ToolExecutor
@@ -181,12 +207,13 @@ public class MavenProvider implements ToolProvider, ToolDetector, ToolProvisione
     @Override
     public ToolExecution.Builder executionTemplate(ToolContext context, Map<String, String> metadata) {
         ToolExecution.Builder builder;
-        String mavenHome = metadata.get(MavenProvider.HOME);
-        if (mavenHome != null) {
+        String jbangHome = metadata.get(GradleProvider.HOME);
+        if (jbangHome != null) {
             builder = ToolExecution.ofCommand(
-                    Path.of(mavenHome).resolve("bin/" + MavenProvider.EXE_NAME).toString());
+                    Path.of(jbangHome).resolve("bin/" + GradleProvider.EXE_NAME).toString());
+            builder.environmentVariable(ENV_HOME, jbangHome);
         } else {
-            builder = ToolExecution.ofCommand(MavenProvider.EXE_NAME);
+            builder = ToolExecution.ofCommand(GradleProvider.EXE_NAME);
         }
 
         return builder;
---
+++ tools/gradle/src/test/java/eu/maveniverse/maven/toolrunner/tools/gradle/GradleProviderTest.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright (c) 2023-2026 Maveniverse Org.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ */
+package eu.maveniverse.maven.toolrunner.tools.gradle;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import eu.maveniverse.maven.toolrunner.shared.Config;
+import eu.maveniverse.maven.toolrunner.shared.ToolHandle;
+import eu.maveniverse.maven.toolrunner.shared.ToolHandler;
+import eu.maveniverse.maven.toolrunner.shared.ToolManager;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import org.junit.jupiter.api.Test;
+
+public class GradleProviderTest {
+    @Test
+    void isolated() throws IOException {
+        // transient = true (clean up after yourself)
+        // allowPathDetection = false (ignore user installed ones)
+        try (ToolManager toolManager = ToolManager.create(Config.builder()
+                .installationDirectory(Path.of("target/installation-directory"))
+                .tempDirectory(Path.of("target/temp-directory"))
+                .isTransient(true)
+                .allowPathDetection(false)
+                .build())) {
+            assertTrue(toolManager.supportedToolNames().contains(GradleProvider.NAME));
+
+            Optional<ToolHandler> maybeHandler = toolManager.selectToolByName("gradle");
+            assertTrue(maybeHandler.isPresent());
+
+            ToolHandler handler = maybeHandler.orElseThrow();
+
+            // detect Gradle, should find zero
+            List<Map<String, String>> detected = handler.detectTool();
+            assertEquals(0, detected.size());
+
+            // provision latest JBang
+            ToolHandle handle = handler.toolHandle();
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            handle.execute(
+                    handle.executionTemplate().argument("-v").stdOut(baos).build());
+            assertTrue(
+                    baos.toString().trim().contains(handle.toolMetadata().get(ToolHandler.TOOL_VERSION)),
+                    baos.toString().trim());
+
+            // detect again, we should have one more provisioned
+            detected = handler.detectTool();
+            assertEquals(1, detected.size());
+        }
+    }
+}

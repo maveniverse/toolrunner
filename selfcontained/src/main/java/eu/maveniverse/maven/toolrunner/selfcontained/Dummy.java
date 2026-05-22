bc69e45cf536e11ca27013599f1d1f7c0e27cdd1
Tamas Cservenak <tamas@cservenak.net>
2026-05-22 15:23:35 +0200

    Fix selfcontained

---
+++ selfcontained/src/main/java/eu/maveniverse/maven/toolrunner/selfcontained/Dummy.java
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2023-2026 Maveniverse Org.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ */
+package eu.maveniverse.maven.toolrunner.selfcontained;
+
+/**
+ * This class is here only to make module have source and javadoc bundle.
+ */
+public class Dummy {}
